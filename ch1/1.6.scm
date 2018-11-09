2a1d3f534d55ebea161082500fe84d0062cc9b4f
antonthomsen <44902380+antonthomsen@users.noreply.github.com>
2018-11-09 23:41:25 +0100

    Update 1.6.scm

--- ch1/1.6.scm
+++ ch1/1.6.scm
@@ -1,1 +1,2 @@
-
+; The new-if is not a special-form. Thus, since Scheme is an applicative-order language,
+; both the if and else clause will be evaluated in the program, and sqrt-iter will be called over and over in an infinite loop.
