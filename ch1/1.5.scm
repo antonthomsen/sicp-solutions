73cbca2e9018482fa252f5526a520eb65b1d424a
antonthomsen <44902380+antonthomsen@users.noreply.github.com>
2018-11-09 22:12:34 +0100

    Update 1.5.scm

--- ch1/1.5.scm
+++ ch1/1.5.scm
@@ -1,1 +1,13 @@
+(define (p) (p))
 
+(define (test x y)
+  (if (= x 0)
+      0
+      y))
+
+(test 0 (p))
+
+; In applicative-order evaluation, succesive functional transformation is applied, to arrive at the result.
+; Here arguments get evaluated first, and thus (p) is evaluated immediately. Since (p) is recursively defined by itself,
+; the program enters an infinite loop. In normal-order evaluation, the arguments (operands) are not evaluated untill needed
+; - thus (p) never gets evaluated, because of the predicate (= x 0)
