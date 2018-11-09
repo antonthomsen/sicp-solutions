69f13bc7b394702eaaf8331f2eff7eb9a5170a88
antonthomsen <44902380+antonthomsen@users.noreply.github.com>
2018-11-09 17:08:25 +0100

    Update 1.3.scm

--- ch1/1.3.scm
+++ ch1/1.3.scm
@@ -1,1 +1,7 @@
-
+; Solution uses cond - could be solved by adding all squares, and then finding (min a b c) and substract the square of the min.
+(define (square x) (* x x))
+(define (f a b c)
+  (cond
+   ((and (> a c) (> b c)) (+ (square a) (square b)))
+   ((and (> a b) (> c b)) (+ (square a) (square c)))
+   ((and (> c a) (> b a)) (+ (square c) (square b)))))
